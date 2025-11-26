dcd375b63bc5931dacb31210159567f8d30b882f
Diego <diegoperez250603@gmail.com>
2025-11-26 08:57:29 -0600

    Version con Componente de Samantha

--- app-angular/src/app/app.routes.ts
+++ app-angular/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { DiegoPerezComponent } from './diego-perez/diego-perez';
 import {AleComponent } from './ale/ale';
 import {Manuel} from './manuel/manuel'
 import { HomeComponent } from './home/home';
+import { SamanthaObregonComponent } from './samantha-obregon/samantha-obregon.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'diego', component: DiegoPerezComponent },
   { path: 'alejandro', component: AleComponent },
-  { path: 'manuel', component: Manuel }
+  { path: 'manuel', component: Manuel },
+  { path: 'samantha', component: SamanthaObregonComponent}
 ];
---
+++ app-angular/src/app/samantha-obregon/samantha-obregon.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-samantha-obregon',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './samantha-obregon.component.html',
+  styleUrl: './samantha-obregon.component.scss',
+})
+export class SamanthaObregonComponent {
+}
+
