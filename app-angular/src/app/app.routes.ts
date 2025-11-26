16e40eba0e4115a7c6e1a34ea56011df2a554a9e
Samantha Mancilla Obregon <23040040@alumno.utc.edu.mx>
2025-11-26 08:14:27 -0600

    Agrego componente Samantha y actualizo rutas y home

--- app-angular/src/app/app.routes.ts
+++ app-angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DiegoPerezComponent } from './diego-perez/diego-perez';
 import { HomeComponent } from './home/home';
+import { SamanthaObregonComponent } from './samantha-obregon/samantha-obregon.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'diego', component: DiegoPerezComponent }
+  { path: 'diego', component: DiegoPerezComponent },
+  { path: 'samantha', component: SamanthaObregonComponent }
 ];
